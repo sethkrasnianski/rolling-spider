0d0136131db0a20403973f60961137fbb1097433
Seth Krasnianski <seth@mojotech.com>
2015-05-30 22:42:36 -0400

    Move flight commands to separate file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import Drone from 'rolling-spider';
 import temporal from 'temporal';
+import flight from 'flight';
 
 const chopper = new Drone();
 
 chopper.connect(() => {
   chopper.setup(() => {
-    temporal.queue([
-      {
-        delay: 0,
-        task: () => {
-          chopper.flatTrim();
-          chopper.startPing();
-          chopper.takeOff();
-        }
-      },
-      {
-        delay: 3000,
-        task: () => {
-          chopper.forward();
-        }
-      },
-      {
-        delay: 500,
-        task: () => {
-          chopper.land();
-        }
-      }
-    ]);
+    temporal.queue(flight);
   });
 });
