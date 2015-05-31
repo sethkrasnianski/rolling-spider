ddb3fd36f528d0cac9dde48a42a975fd65c30e7f
Seth Krasnianski <seth@mojotech.com>
2015-05-30 22:43:07 -0400

    Build 0.0.2

--- build/index.js
+++ build/index.js
@@ -10,27 +10,14 @@ var _temporal = require('temporal');
 
 var _temporal2 = _interopRequireDefault(_temporal);
 
+var _flight = require('flight');
+
+var _flight2 = _interopRequireDefault(_flight);
+
 var chopper = new _rollingSpider2['default']();
 
 chopper.connect(function () {
   chopper.setup(function () {
-    _temporal2['default'].queue([{
-      delay: 0,
-      task: function task() {
-        chopper.flatTrim();
-        chopper.startPing();
-        chopper.takeOff();
-      }
-    }, {
-      delay: 3000,
-      task: function task() {
-        chopper.forward();
-      }
-    }, {
-      delay: 500,
-      task: function task() {
-        chopper.land();
-      }
-    }]);
+    _temporal2['default'].queue(_flight2['default']);
   });
 });
